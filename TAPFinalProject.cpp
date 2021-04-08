cc3640eb02ed2fe658d0e016408a8f4123d59768
Trevor Pierce <yeahtrevorpierce@gmail.com>
2021-04-07 23:40:38 -0400

    stuff

--- TAPFinalProject.cpp
+++ TAPFinalProject.cpp
@@ -13,33 +13,36 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-#define NUM_PEOPLE 6
-
-struct person_t {
+struct person_t
+{
     string name;
     string email;
 };
 
-person_t roster[NUM_PEOPLE] = {
-    (person_t){"Diana", "dnixon4@ivytech.edu"},
-    (person_t){"Andy", "andy@ivytech.edu"},
-    (person_t){"Beth", "beth@ivytech.edu"},
-    (person_t){"Henry", "henry@ivytech.edu"},
-    (person_t){"Lily", "lili@ivytech.edu"},
-    (person_t){"Trevor", "tpierce42@ivytech.edu"}
+vector<person_t> roster =
+{
+    {"Diana", "dnixon4@ivytech.edu"},
+    {"Andy", "andy@ivytech.edu"},
+    {"Beth", "beth@ivytech.edu"},
+    {"Henry", "henry@ivytech.edu"},
+    {"Lily", "lili@ivytech.edu"},
+    {"Trevor", "tpierce42@ivytech.edu"}
 };
 
-enum class Permissions {
+enum class Permissions
+{
     student,
     faculty,
     staff
 };
 
 // This class will be used to store information about a user that logs in
-class User {
+class User
+{
     private:
         string _name, _password;
         Permissions _permissions; // This will tell if the user is a teacher or student
@@ -56,45 +59,60 @@ class User {
         string getGrades();
 };
 
-void User::setName(string name) {
+void User::setName(string name)
+{
     _name = name;
 }
 
-string User::getName() {
+string User::getName()
+{
     return _name;
 }
 
-void User::setPassword(string password) {
+void User::setPassword(string password)
+{
     _password = password;
 }
 
-string User::getPassword() {
+string User::getPassword()
+{
     return _password;
 }
 
-void User::setPermissions(string permissions) {
-    if (permissions == "S") {
+void User::setPermissions(string permissions)
+{
+    if (permissions == "S")
+    {
         _permissions = Permissions::student;
-    } else if (permissions == "F") {
+    } 
+    else if (permissions == "F") 
+    {
         _permissions = Permissions::faculty;
-    } else if (permissions == "AS") {
+    }
+    else if (permissions == "AS")
+    {
         _permissions = Permissions::staff;
-    } else {
+    }
+    else
+    {
         cerr << "Invalid permissions set" << endl;
         cerr << "Current permissions set to: " << permissions << endl;
         // Need to find a way to exit the program here...
     }
 }
 
-Permissions User::getPermissions() {
+Permissions User::getPermissions()
+{
     return _permissions;
 }
 
-void User::setGrades(string grades) {
+void User::setGrades(string grades)
+{
     _grades = grades;
 }
 
-string User::getGrades() {
+string User::getGrades()
+{
     return _grades;
 }
 
@@ -106,7 +124,8 @@ void viewRoster();
 void viewGrades(User &);
 void sendEmail(User &);
 
-int main() {
+int main()
+{
     // Time. The method in the book always gave me random results for some reason, but I found this method online and it always works perfectly due to time(0);
     time_t now = time(0);
     tm *localtm = localtime(&now);
@@ -124,10 +143,12 @@ int main() {
     userFile.open(fileName, ios::in); // Opens the user's data file for reading
     logFile.open("log.txt", ios::app); // Opens the log file to append new data to the end of the current file
 
-    if (userFile) {
+    if (userFile)
+    {
         getline(userFile, input); // Gets the first line of the user's data file first
 
-        while (userFile) { // Until the end of the file
+        while (userFile) 
+        {
             updateUserInfo(currentUser, input, fileLine); // Calls a function that updates the User class instance with the data in the user file, line by line
             getline(userFile, input);
             fileLine++;
@@ -136,8 +157,10 @@ int main() {
         cout << "Please enter your password: ";
         cin >> userPassword;
 
-        if (checkPassword(currentUser, userPassword)) { // This checks is the password the user entered is correct. The password is line 2 of your user data file
-            while (!logout) { // If the user hasn't pressed 4
+        if (checkPassword(currentUser, userPassword))
+        {
+            while (!logout) // If the user hasn't pressed 4
+            {
                 system("CLS"); // This clears the screen
                 cout << "1. View Class Roster\n";
                 cout << "2. View Your Grades\n";
@@ -147,7 +170,8 @@ int main() {
                 cin >> menuChoice;
                 system("CLS");
 
-                switch (menuChoice) {
+                switch (menuChoice)
+                {
                     case 1: viewRoster(); // Calls a function to allow the user to view the class roster
                         logFile << currentUser.getName() << " Action: " << menuChoice << " Time/Date: " << asctime(localtm) << endl; // This writes to the log the user, their action, and the time/date
                         break;
@@ -164,10 +188,14 @@ int main() {
                     default: break;
                 }
             }
-        } else {
+        }
+        else
+        {
             cerr << "The password that you entered was incorrect and now the program will close";
         }
-    } else {
+    }
+    else 
+    {
         cerr << "Oh no! It looks like you do not have an account in our database!\n";
         cerr << "Please contact your administrator if you think that this is an error";
     }
@@ -176,8 +204,10 @@ int main() {
     return EXIT_SUCCESS;
 }
 
-void updateUserInfo(User &currentUser, string input, int fileLine) {
-    switch (fileLine) {
+void updateUserInfo(User &currentUser, string input, int fileLine)
+{
+    switch (fileLine)
+    {
         case 1: currentUser.setName(input);
             break;
         case 2: currentUser.setPassword(input);
@@ -190,17 +220,23 @@ void updateUserInfo(User &currentUser, string input, int fileLine) {
 }
 
 // This function checks to see if the user entered the password matching the one stored in the data file. I know, not safe, but this is what I did. I should have looked into hashing the passwords
-bool checkPassword(User &currentUser, string userPassword) {
-    if (currentUser.getPassword() == userPassword) {
+bool checkPassword(User &currentUser, string userPassword)
+{
+    if (currentUser.getPassword() == userPassword)
+    {
         return true;
-    } else {
+    }
+    else
+    {
         return false;
     }
 }
 
 // This function displays the names and emails of people in the class
-void viewRoster() {
-    for (person_t person : roster) {
+void viewRoster() 
+{
+    for (person_t person : roster)
+    {
         cout << "Name: " << person.name;
         cout << " | Email: " << person.email;
         cout << endl;
@@ -210,34 +246,41 @@ void viewRoster() {
 }
 
 // This function allows the user to view their grades if they're a student. If a teacher, then they are told they cannot view their grades
-void viewGrades(User &currentUser) {
-    if (currentUser.getPermissions() == Permissions::student) {
+void viewGrades(User &currentUser)
+{
+    if (currentUser.getPermissions() == Permissions::student)
+    {
         string grades = currentUser.getGrades();
 
         // This whole block goes character by character and looks for $ delimiters that I placed between values in the string, then prints a new line
         // and moves onto the next iteration. This is because I had a very hard time get private class arrays to work right and I got this to work
         string currentChar;
 
-        for (char grade : grades) {
+        for (char grade : grades)
+        {
             currentChar = grade;
 
-            if (currentChar == "$") { // I put $'s to separate values in the string
+            if (currentChar == "$")
+            { // I put $'s to separate values in the string
                 cout << endl;
                 continue; // This skips to the next iteration of the loop
-            } else {
+            }
+            else
+            {
                 cout << currentChar; // This prints out the current character at the current index in the string
             }
         }
-    } else if (currentUser.getPermissions() == Permissions::faculty) {
-        cerr << "Faculty cannot view grades since they do not have assignments";
-    } else if (currentUser.getPermissions() == Permissions::staff) {
-        cerr << "Adminstrative staff cannot view grades since they do not have assignments";
+    } 
+    else
+    {
+        cerr << "Only students can view grades, since only they have assignments";
     }
     cout << endl;
     system("pause");
 }
 
-void sendEmail(User &currentUser) {
+void sendEmail(User &currentUser)
+{
     cin.ignore(); // You have to use this to ignore the previous \n that might be in the input buffer, otherwise the program skips asking for the email and asks for the recipient
     string email, receiver;
 
@@ -250,16 +293,21 @@ void sendEmail(User &currentUser) {
     // The following block is a linear search to see if the program can find the user that you entered
     bool found = false;
 
-    for (person_t person : roster) {
-        if (person.name == receiver) {
+    for (person_t person : roster)
+    {
+        if (person.name == receiver)
+        {
             found = true;
             break;
         }
     }
 
-    if (found) {
+    if (found)
+    {
         cout << "Email has been sent from " << currentUser.getName() << " to " << receiver;
-    } else {
+    }
+    else
+    {
         cerr << "Sorry, we could not find " << receiver << " in our database";
     }
     cout << endl;
