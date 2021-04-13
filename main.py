b82235053e6cf6cf15aeb214d62cbe79ba71d858
Trevor Pierce <yeahtrevorpierce@gmail.com>
2021-04-12 20:57:52 -0400

    Added some system code

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import logging
-
 from models import User
 import system
 
 if __name__ == "__main__":
-    logging.basicConfig(filename="sis.log", encoding="utf-8", level=logging.INFO)
+    system.startup()
     current_user = system.login()
--- system.py
+++ system.py
@@ -1,14 +1,34 @@
+import logging
+from os import environ
+
 import psycopg2
 
 from models import Permissions
 from models import User
 
 class Config():
-    _db_name = "test"
-    _db_user = "postgres"
-    _db_secret = ""
-    db_url = f"dbname={_db_name} user={_db_user} secret={_db_secret}"
+    db: psycopg2.connection
 
+    def load(self) -> None:
+        logging.basicConfig(
+            filename="sis.log",
+            encoding="utf-8",
+            level=logging.INFO
+        )
+        logging.info("System starting up...")
+        logging.info("Connecting to database...")
+        self.db = psycopg2.connect(
+            db_name=environ["PGDATABASE"],
+            user=environ["PGUSER"],
+            password=environ["PGPASSWORD"],
+            host=environ["PGHOST"],
+            port=environ["PGPORT"]
+        )
+        logging.info("Connected to database successfully")
+
+
+def startup() -> None:
+    Config.load()
 
 def login() -> User:
     username_is_invalid = True
@@ -17,7 +37,6 @@ def login() -> User:
     while username_is_invalid:
         entered_username = input("Hello! Please enter your username: ")
         username_is_invalid = False
-        conn = psycopg2.connect(Config.db_url)
         current_user = User(entered_username, f"{entered_username}@test.edu", Permissions.STUDENT)
         break
 
