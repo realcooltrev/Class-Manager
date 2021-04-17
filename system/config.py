e2715086df98a6435773766d826722b449a81fe7
Trevor Pierce <yeahtrevorpierce@gmail.com>
2021-04-17 17:40:51 -0400

    Removed pg connection code and logging code in system.config and fixed invalid ConfigParser reference

--- system/config.py
+++ system/config.py
@@ -1,5 +1,4 @@
 from configparser import ConfigParser
-import logging
 from os import environ
 
 import psycopg2
@@ -9,13 +8,8 @@ class Config():
 
     @classmethod
     def load(cls) -> None:
-        logging.info("Connecting to database...")
-        cls.db = psycopg2.connect(
-            db_name=environ["PGDATABASE"],
-            user=environ["PGUSER"],
-            password=environ["PGPASSWORD"],
-            host=environ["PGHOST"],
-            port=environ["PGPORT"]
-        )
-        logging.info("Connected to database successfully")
+        config = ConfigParser()
+        config.read("../config.ini")
+        cls.db = config["db"]
+
 
