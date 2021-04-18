fcf99687abe86eb75a0e12ffed9dd9413635e55a
Trevor Pierce <yeahtrevorpierce@gmail.com>
2021-04-17 20:23:36 -0400

    Starting work also on getting unit testing setup

--- system/config.py
+++ system/config.py
@@ -1,15 +1,24 @@
 from configparser import ConfigParser
-from os import environ
+from enum import Enum
 
 import psycopg2
 
+
+class Env(Enum):
+    PROD = "prod"
+    TEST = "test"
+
+
 class Config():
     db: any
+    environment: Env
 
     @classmethod
-    def load(cls) -> None:
+    def load(cls, environment: Env) -> None:
+        cls.environment = environment
+
         config = ConfigParser()
-        config.read("../config.ini")
+        config.read(f"../config_{environment}.ini")
         cls.db = config["db"]
 
 
---
+++ test_app.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class TestApp(unittest.TestCase):
+    pass
+
+
+class TestConfig(unittest.TestCase):
+    pass
+
+
+class TestDb(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
